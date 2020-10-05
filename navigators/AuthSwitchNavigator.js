f5b10882b1fc86acec33ede2fa66bce66f06696b
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-10-05 15:53:06 -0400

    Remove unused imports

--- navigators/AuthSwitchNavigator.js
+++ navigators/AuthSwitchNavigator.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Icon } from 'native-base';
 import { createSwitchNavigator } from 'react-navigation';
-
 import RegisterScreen from '../ios/src/components/register';
 import LoginScreen from '../ios/src/components/login';
 
